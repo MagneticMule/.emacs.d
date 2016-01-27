f21f77d2f12144971e87533bce0f7a7ebe0dc3ef
Thomas Sweeney <skywriter@gmail.com>
2016-01-27 15:40:01 +0000

    Evil mode - it begins!

--- my-package-loader.el
+++ my-package-loader.el
@@ -68,8 +68,6 @@
 ;; (add-hook 'mail-mode-hook 'ac-ispell-ac-setupa)
 
 
-;;(require 'magit)
-
 
 (require 'ido)
 (setq ido-enable-flex-matching t
@@ -122,8 +120,8 @@
 (require 'powerline)
 (powerline-center-theme)
 
-;;(require 'helm)
-;;(require 'helm-config)
+(require 'helm)
+(require 'helm-config)
 
 ;; (require 'helm-files)
 ;; (require 'helm-grep)
@@ -134,35 +132,35 @@
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-;;(global-set-key (kbd "C-c h") 'helm-command-prefix)
-;;(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
 
-;;(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
-;;(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-;;(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-;;(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
-;;(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
-;;(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
 
 (when (executable-find "curl")
 (setq helm-google-suggest-use-curl-p t))
 
-;; (setq helm-quick-update                     t ; do not display invisible candidates
-;;       helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-;;       helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
-;;       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-;;       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-;;       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-;;       helm-ff-file-name-history-use-recentf t)
+ (setq helm-quick-update                     t ; do not display invisible candidates
+       helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+       helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+       helm-ff-file-name-history-use-recentf t)
 
-;;(helm-mode 1)
+(helm-mode 1)
 
 
 
-;; (require 'helm-flycheck)
-;; (eval-after-load 'flycheck
-;;   '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
+ (require 'helm-flycheck)
+ (eval-after-load 'flycheck
+   '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
 
 
 ;; (setq reftex-plug-into-AUCTeX t)
@@ -258,8 +256,23 @@
 (when (memq window-system '(mac ns))
     (exec-path-from-shell-initialize))
 
-;; (require 'evil-mode)
+;; smart-parens bracket matching
+(smartparens-global-mode 1)
+
+;; evil mode
+(require 'evil)
+(evil-mode 1)
+(setq evil-leader/in-all-states 1)
+(global-evil-leader-mode)
+
+;; emmet mode
+(require 'emmet-mode)
+(emmet-mode 1)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
 
 
 (provide 'my-package-loader)
+
+
 ;;; my-package-loader ends here
